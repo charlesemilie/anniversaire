06ab18014b57dcbdbb087a301260ef34c6d637e5
charlesemilie <charlesduffour2003@gmail.com>
2025-05-17 21:47:24 +0200

    Mise à jour password et redirection

--- js/password.js
+++ js/password.js
@@ -1,18 +1,11 @@
-console.log("password.js chargé");
-
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("btnValider");
-
-  btn.addEventListener("click", () => {
-    console.log("bouton cliqué");
-    const motdepasseInput = document.getElementById("motdepasse").value.trim();
-    const motdepasseCorrect = "test";
-
-    if (motdepasseInput === motdepasseCorrect) {
-      console.log("Mot de passe correct, redirection...");
-      window.location.href = "accueil.html";
+function checkPassword() {
+    const input = document.getElementById('passwordInput').value;
+    console.log("Mot de passe saisi :", input);
+    if (input === "test") {
+        console.log("Mot de passe correct, redirection...");
+        window.location.href = "accueil.html";
     } else {
-      alert("Mot de passe incorrect, essaie encore !");
+        console.log("Mot de passe incorrect");
+        alert("Mot de passe incorrect");
     }
-  });
-});
+}
